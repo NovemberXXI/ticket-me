4b70d966b5206c8515a6a968ac0c2c1d6d384457
Edmond Setati <NovemberXXI.Business@gmail.com>
2023-08-11 11:58:22 +0200

    event display added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,41 +1,42 @@
 import React, { useState } from 'react'
-import { FaCalendar, FaCalendarDay} from 'react-icons/fa'
-import Suite from './assets/suite.jpg'
-import {DateRange} from 'react-date-range'
-import 'react-date-range/dist/styles.css';
-import 'react-date-range/dist/theme/default.css';
-import { format } from 'date-fns';
+import { FaCalendarDay, FaSearch} from 'react-icons/fa'
+
+import { data } from '../data/database';
+
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 
 const Home = () => {
+  const [event] =useState(data);
 
-  const [openDate, setOpenDate] = useState(false)
+  const [selectedDate, setSelectedDate] = useState(null)
 
-  const [date, setDate] = useState([
-    {
-      startDate: new Date(),
-      key: 'selection'
-    }
-  ]);
   return (
     <div className='w-full h-auto bg-gray-400 py-20'>
         {/* calender and search event */}
         <div className='flex flex-col max-w-[500px] md:max-w-[800px] justify-between items-center text-center py-8 bg-gray-500 px-6 mx-auto'>
 
-          <div className='flex gap-1 items-center justify-center absolute py-4 top-20'>
-              <FaCalendar size={10}/>
-              <input type="text" placeholder='Search event' className='border-4 outline-none w-[180px] sm:w-[300px] text-center shadow-black' />
+          <div className='flex gap-2 items-center justify-center absolute py-4 top-20'>
+              <FaSearch size={15}/>
+              <input type="text" placeholder='Search event' className='border-4 outline-none sm:w-[300px] text-center shadow-black' />
           </div>
-          <div className='flex gap-1 items-center py-5 justify-center cursor-pointer'>
-              <FaCalendarDay size={10}/>
-              <span onClick={()=>setOpenDate(!openDate)} className='text-gray-300 py-2'>{`${format(date[0].startDate, "dd/MM/yyyy")}`}</span>
-              {openDate && <DateRange
-                editableDateInputs={true}
-                onChange={item => setDate([item.selection])}
-                moveRangeOnFirstSelection={false}
-                ranges={date}
+          <div className='flex gap-2 items-center py-5 justify-center cursor-pointer'>
 
-                className='absolute top-[170px]'
-              />}
+              <FaCalendarDay size={15}/>
+              <span className='text-gray-500 py-2 text-center justify-center items-center'>
+                <DatePicker 
+                selected={selectedDate} 
+                onChange={ date => setSelectedDate(date)} 
+                dateFormat='dd/MM/yyyy'
+                minDate={new Date()}
+                showYearDropdown
+                scrollableYearDropdown
+                
+                className=' text-center outline-none'/>
+              </span>
+
+              
           </div>
         </div>
 
@@ -45,21 +46,20 @@ const Home = () => {
               <div className='py-4 mx-8' >
                 <h1 className='text-center text-gray-200 py-2 mx-2 justify-center items-center text-2xl inline-block'>Events</h1>
               </div>
-            <div className='justify-center items-center mx-auto'>
+            <div className='justify-center items-center mx-auto flex '>
+              <div className='sm:grid md:grid grid-cols-3'>
 
-              {/* event pic display */}
-              <div className='grid sm:grid-cols-3 items-center justify-center text-center'>
-                {/* event pics */}
-                
-                  <img src={Suite} alt="siute ticket" className='w-[300px] h-[150px] m-2 px-3 mx-auto'/>
-                  <img src={Suite} alt="siute ticket" className='w-[300px] h-[150px] m-2 px-3 mx-auto'/>
-                  <img src={Suite} alt="siute ticket" className='w-[300px] h-[150px] m-2 px-3 mx-auto'/>
-                
-                
-                {/* event pics */}
+                  {/* event pic display */}
+                  {event.map((item, index) =>(
+                    <div key={index}>
 
+                      <img src={item.eventimage} alt={item.eventimage} className='w-[300px] h-[150px] m-2 px-3 pb-6 mx-auto md:w-[550px] md:h-[200px] object-cover'/>
+                    
+                    </div>
+                  ))}
+                </div>
               </div>
-            </div>
+            
         </div>
     </div>
 
---
+++ src/data/database.js
@@ -0,0 +1,95 @@
+export const data = [
+  {
+    id: 1,
+    name: "Maphorisa tour",
+    category: "Amapiano",
+    price: "R150",
+    eventimage:
+      "https://cdn-az.allevents.in/events5/banners/79d8738dbc4a864563a34d2ded1cf3fffbe480571f2a5afcb9941b1f8f5e6bea-rimg-w526-h526-gmir.jpg?v=1629225306",
+  },
+  {
+    id: 2,
+    name: "Major league rooftop",
+    category: "Amapiano",
+    price: "R150",
+    eventimage:
+      "https://zatunes.co.za/wp-content/uploads/2022/12/tickets-on-sale_orig.jpg",
+  },
+  {
+    id: 3,
+    name: "DJ Stokie event",
+    category: "Amapiano",
+    price: "R150",
+    eventimage:
+      "https://cdn-az.allevents.in/events7/banners/7b736ea3d81a9d789dd22a5f034ae0bc345cffad33311139163a547fbdb7e659-rimg-w960-h540-gmir.jpg?v=1691447370",
+  },
+  {
+    id: 4,
+    name: "Kabza De Small konka",
+    category: "Amapiano",
+    price: "R150",
+    eventimage:
+      "https://images.pexels.com/photos/1540406/pexels-photo-1540406.jpeg?auto=compress&cs=tinysrgb&w=600",
+  },
+  {
+    id: 5,
+    name: "Black Motion tour",
+    category: "Amapiano",
+    price: "R150",
+    eventimage:
+      "https://images.pexels.com/photos/1763067/pexels-photo-1763067.jpeg?auto=compress&cs=tinysrgb&w=600",
+  },
+  {
+    id: 6,
+    name: "Cassper Nyovest",
+    category: "HipHop",
+    price: "R150",
+    eventimage:
+      "https://images.pexels.com/photos/2263436/pexels-photo-2263436.jpeg?auto=compress&cs=tinysrgb&w=600",
+  },
+  {
+    id: 7,
+    name: "K.O",
+    category: "HipHop",
+    price: "R150",
+    eventimage:
+      "https://images.pexels.com/photos/2034851/pexels-photo-2034851.jpeg?auto=compress&cs=tinysrgb&w=600",
+  },
+  {
+    id: 8,
+    name: "A-Reece",
+    category: "HipHop",
+    price: "R150",
+    eventimage:
+      "https://images.pexels.com/photos/1238978/pexels-photo-1238978.jpeg?auto=compress&cs=tinysrgb&w=600",
+  },
+  {
+    id: 9,
+    name: "Blxckie",
+    category: "HipHop",
+    price: "R150",
+    eventimage:
+      "https://images.pexels.com/photos/1238980/pexels-photo-1238980.jpeg?auto=compress&cs=tinysrgb&w=600",
+  },
+];
+
+// categories
+
+export const categories = [
+  {
+    id: 1,
+    name: "Hip Hop",
+  },
+  {
+    id: 1,
+    name: "Amapiano",
+  },
+  {
+    id: 1,
+    name: "Deep House",
+  },
+  {
+    id: 1,
+    name: "Afro Beats",
+  },
+];
